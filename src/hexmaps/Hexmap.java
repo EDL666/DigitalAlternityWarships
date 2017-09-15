59818083605ffbd38834f6579ae7678b3af18cce
EDL666 <samuellachance@hotmail.com>
2017-09-15 13:22:19 -0400

    Push init

---
+++ src/hexmaps/Hexmap.java
@@ -0,0 +1,6 @@
+package hexmaps;
+
+public class Hexmap
+{
+	
+}
---
+++ tests/hexmapsTests/HexmapTests.java
@@ -0,0 +1,15 @@
+package hexmapsTests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class HexmapTests
+{
+	
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+	
+}
